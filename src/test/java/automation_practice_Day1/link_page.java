8ce5e72bcebc1c21fd12ba975cdf63bc2fe5f6b1
Anurag <apatel665@gmail.com>
2026-03-12 10:57:31 +0530

    rb test script added

---
+++ src/test/java/automation_practice_Day1/link_page.java
@@ -0,0 +1,38 @@
+package automation_practice_Day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.cucumber.messages.types.Duration;
+import java.util.List;
+
+public class link_page {
+
+	public static void main(String[] args) {
+		
+//Navigate to page and verify title of page
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://testautomationpractice.blogspot.com/");
+		
+// VERIFY ALL LINK ON PAGE  
+		   List<WebElement>Links=driver.findElements(By.tagName("a"));
+		   System.out.println("Total No Of link:"+Links.size());
+		
+// Re-find links  and perform click action
+		        for (int i = 0; i < Links.size(); i++) {
+			    Links = driver.findElements(By.tagName("a"));
+			    String linkText = Links.get(i).getText();
+			    System.out.println("Clicking link: " + linkText);
+			    Links.get(i).click();
+			    
+// Go back to main page
+			    driver.navigate().back(); 
+			}
+		   
+	}
+		
+	}
+
+
--- src/test/java/automation_practice_Day1/rediobtn.java
+++ src/test/java/automation_practice_Day1/rediobtn.java
@@ -1,24 +1,30 @@
 package automation_practice_Day1;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
 
-public class title {
-	
+public class rediobtn {
 
 	public static void main(String[] args) {
-		
-//Navigate to page and verify title of page
+
+//navigate to the page
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://testautomationpractice.blogspot.com/");
-		System. out.println("Title of The Web Page:" + driver.getTitle());
-	   
-		
-		
-		   
-	}
+			
+//locate element and perform action
+		List<WebElement> rediobtnList = driver.findElements(By.xpath("//input[@type='radio']"));
 
+		for(WebElement radio : rediobtnList) {
+			 if(radio.getAttribute("id").equals("female")){
+			        radio.click();
+			        
+		    System.out.println(radio.getAttribute("id"));
+			 }
+		}
+	}
 }
+
